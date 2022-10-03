130273016cb51d23514d61766824918cfba9a405
Doretto <dorettoartesp@gmail.com>
2022-10-02 22:08:39 -0300

    refactor

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env sh
 
 # abort on errors
-set -e
+# set -e
 
-# build
-npm run build
+# # build
+# npm run build
 
-# navigate into the build output directory
-cd dist
+# # navigate into the build output directory
+# cd dist
 
-# place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
+# # place .nojekyll to bypass Jekyll processing
+# echo > .nojekyll
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# # if you are deploying to a custom domain
+# # echo 'www.example.com' > CNAME
 
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
+# git init
+# git checkout -b main
+# git add -A
+# git commit -m 'deploy'
 
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
